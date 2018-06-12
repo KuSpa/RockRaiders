94aaf8599c07c3b1d9bef58e75555fb487710125
KuSpa <wil.friedemann@gmail.com>
2018-06-12 15:39:54 +0200

    window creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+extern crate amethyst;
+
+use amethyst::Result;
+use amethyst::prelude::*;
+use amethyst::renderer::{DisplayConfig, DrawFlat, Event, KeyboardInput,
+                         Pipeline, PosTex, RenderBundle, Stage, 
+                         VirtualKeyCode, WindowEvent};
+
+struct RockRaider; 
+impl<'a, 'b> State<GameData<'a, 'b>> for RockRaider {
+    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => Trans::Quit,
+                _ => Trans::None,
+            },
+            _ => Trans::None,
+        }
+    }
+
+    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        data.data.update(&data.world);
+        Trans::None
+    }
+}
+
+fn run() -> Result<()> {
+    let path = "./resources/display_config.ron";
+
+    let config = DisplayConfig::load(&path);
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosTex>::new()),
+    );
+
+    let game_data = GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let mut game = Application::new("./", RockRaider, game_data)?;
+    game.run();
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, RockRaiders!");
+    if let Err(e) = run() {
+        println!("Error occurred during game execution: {}", e);
+        ::std::process::exit(1);
+    }
 }
