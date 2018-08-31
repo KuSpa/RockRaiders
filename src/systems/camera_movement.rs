de3ef01b522855f124d1007eb9ae9078a268f251
Wilhelm Friedemann <wil.friedemann@gmail.com>
2018-08-31 18:49:21 +0200

    added screenspecs

--- src/systems/camera_movement.rs
+++ src/systems/camera_movement.rs
@@ -1,11 +1,11 @@
-use amethyst::core::specs::prelude::{Join, Read, ReadStorage, System, WriteStorage};
+use amethyst::core::specs::prelude::{Join, Read, ReadExpect, ReadStorage, System, WriteStorage};
 use amethyst::core::timing::Time;
 use amethyst::core::transform::Transform;
 use amethyst::input::InputHandler;
 
 use amethyst::core::cgmath::prelude::Zero;
 use amethyst::core::cgmath::Vector3;
-use amethyst::renderer::{Camera, VirtualKeyCode};
+use amethyst::renderer::{Camera, ScreenDimensions};
 
 pub struct CameraMovementSystem;
 
@@ -15,37 +15,30 @@ impl<'a> System<'a> for CameraMovementSystem {
         WriteStorage<'a, Transform>,
         Read<'a, InputHandler<String, String>>,
         ReadStorage<'a, Camera>,
+        ReadExpect<'a, ScreenDimensions>,
     );
 
-    fn run(&mut self, (time, mut transforms, input, cam): Self::SystemData) {
+    fn run(&mut self, (time, mut transforms, input, cam, screen_size): Self::SystemData) {
         let (mut x, mut z) = (0.0, 0.0);
-        //TODO use window settings!!!
+        //TODO = 20 as magic number in .ron spec file?
+        //       Offset to borders as speed? for a smoother feeling?
+
         if let Some((mouse_x, mouse_y)) = input.mouse_position() {
             //X value of the mouse
             if mouse_x < 20.0 {
                 x -= 1.0;
-            } else if mouse_x > 480.0 {
+            } else if mouse_x > (screen_size.width() - 20.0).into() {
                 x += 1.0;
             }
 
             //Y value of the mouse === Z movement of the camera
             if mouse_y < 20.0 {
                 z -= 1.0;
-            } else if mouse_y > 480.0 {
+            } else if mouse_y > (screen_size.height() - 20.0).into() {
                 z += 1.0;
             }
         }
 
-        // Keep this for debugging purposes
-        //for pressed_key in input.keys_that_are_down() {
-        //    match pressed_key {
-        //        VirtualKeyCode::W => z -= 1.0,
-        //        VirtualKeyCode::S => z += 1.0,
-        //        VirtualKeyCode::A => x -= 1.0,
-        //        VirtualKeyCode::D => x += 1.0,
-        //        _ => (),
-        //    }
-        //}
         let dir = Vector3::new(x, 0.0, z);
         info!("{:?}", dir);
         if !dir.is_zero() {
