4a1e2d4de1d651f7cc4c637c872873c8f1b5099f
Wilhelm Friedemann <wilhelm.friedemann@student.hpi.de>
2018-09-03 09:08:09 +0200

    asdf

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() -> amethyst::Result<()> {
 
     // This line is not mentioned in the pong tutorial as it is specific to the context
     // of the git repository. It only is a different location to load the assets from.
-    let assets_dir = format!("{}/", env!("CARGO_MANIFEST_DIR"));
+    let assets_dir = format!("{}/assets", env!("CARGO_MANIFEST_DIR"));
 
     let input = InputBundle::<String, String>::new();
 
@@ -34,7 +34,8 @@ fn main() -> amethyst::Result<()> {
             systems::CameraMovementSystem,
             "camera_movement_system",
             &["input_system"],
-        ).with_bundle(TransformBundle::new())?
+        )
+        .with_bundle(TransformBundle::new())?
         .with_basic_renderer(path, DrawFlat::<PosTex>::new(), false)?;
     let mut game = Application::new(assets_dir, RockRaiders, game_data)?;
     game.run();
